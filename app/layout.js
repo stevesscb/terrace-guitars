01e14b513edfa674d7f158d7a9b7646438e7a83b
Stephen <stevesscb@gmail.com>
2024-03-01 14:42:52 -0500

    Add hero section

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import Container from '@/app/ui/container/Container';
 
 import { Inter } from 'next/font/google';
 
-import './globals.css';
+import './globals.scss';
 
 const inter = Inter({ subsets: ['latin'] });
 
---
+++ app/lib/placeholder-data.js
@@ -0,0 +1,9 @@
+export const guitars = [
+  {
+    make: 'Fender',
+    model: 'Stratocaster',
+    year: '1990',
+    image:
+      'https://therockinn.com/cdn/shop/files/MG_9974_0b779385-019c-449f-99c7-aeae2a8265f0_1024x1024.jpg?v=1705973038',
+  },
+];
